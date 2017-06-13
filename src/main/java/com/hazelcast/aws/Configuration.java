13a8a8073da8fcd94972479bf7d6132274d58540
baris <baris.aydinoz@gmail.com>
2017-06-13 11:58:38 +0300

    fixes for checkstyle errors

--- src/main/java/com/hazelcast/aws/Configuration.java
+++ src/main/java/com/hazelcast/aws/Configuration.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.aws;
-
-
 
 public class Configuration {
     private static final int CONNECTION_TIMEOUT = 10;
@@ -95,7 +109,7 @@ public class Configuration {
     }
 
     public Configuration setConnectionTimeoutSeconds(int connectionTimeoutSeconds) {
-        if(connectionTimeoutSeconds < 0) {
+        if (connectionTimeoutSeconds < 0) {
             throw new IllegalArgumentException("connection timeout can\'t be smaller than 0");
         } else {
             this.connectionTimeoutSeconds = connectionTimeoutSeconds;
@@ -113,6 +127,9 @@ public class Configuration {
     }
 
     public String toString() {
-        return "Configuration{enabled=" + this.enabled + ", region=\'" + this.region + '\'' + ", securityGroupName=\'" + this.securityGroupName + '\'' + ", tagKey=\'" + this.tagKey + '\'' + ", tagValue=\'" + this.tagValue + '\'' + ", hostHeader=\'" + this.hostHeader + '\'' + ", iamRole=\'" + this.iamRole + '\'' + ", connectionTimeoutSeconds=" + this.connectionTimeoutSeconds + '}';
+        return "Configuration{enabled=" + this.enabled + ", region=\'" + this.region + '\'' + ", securityGroupName=\'" +
+                this.securityGroupName + '\'' + ", tagKey=\'" + this.tagKey + '\'' + ", tagValue=\'" + this.tagValue +
+                '\'' + ", hostHeader=\'" + this.hostHeader + '\'' + ", iamRole=\'" + this.iamRole + '\'' +
+                ", connectionTimeoutSeconds=" + this.connectionTimeoutSeconds + '}';
     }
 }
