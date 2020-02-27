cabd0a95e164f605d1a73260875588a775105a18
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-02-27 07:35:59 +0100

    final before transient

--- src/main/java/com/hazelcast/aws/AwsProperties.java
+++ src/main/java/com/hazelcast/aws/AwsProperties.java
@@ -102,7 +102,7 @@ public enum AwsProperties {
      */
     PORT("hz-port", STRING, true);
 
-    private transient final PropertyDefinition propertyDefinition;
+    private final transient PropertyDefinition propertyDefinition;
 
     AwsProperties(String key, PropertyTypeConverter typeConverter, boolean optional, ValueValidator validator) {
         this.propertyDefinition = new SimplePropertyDefinition(key, optional, typeConverter, validator);
